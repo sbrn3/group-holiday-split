d5e8113d39c03df754ddcdc7b3f8e40e21ae6ce3
sbrn3 <Australia555>
2019-10-21 15:29:20 +1000

    Update readme

--- README.md
+++ README.md
@@ -1,7 +1,38 @@
 # group-holiday-split
-Have you ever booked out an apartment or house as part of a group holiday? A common problem is figuring out the prices that everyone should pay especially if the rooms have different utilities. 
+Have you ever booked out an apartment or house as part of a group holiday? A common problem is figuring out the prices 
+that everyone should pay especially if the rooms have different utilities. 
 
-This is extremely similar to a rent splitting strategy except there are a few variables that may be different in this particular case.
+This is extremely similar to a rent splitting strategy except there are a few variables that may be different in this 
+particular case.
 Are there people that would prefer to be in the same room but could technically be split (couples). 
 
-Also people may not be requiring accomodation for all the nights and therefore space might be wasted if they don't plan 
+Also people may not be requiring accomodation for all the nights and therefore space might be wasted if they don't plan
+on using a good room for all the nights.  
+
+# Rules of the auction
+
+1. Bid a value on each bed type. 
+
+2. The total of your bids must equal (or more than) the cost per night for the entire house. 
+
+# Result
+
+If you bid the highest then you get that room. If you have valued the room more than other people then price you pay is 
+the average of everyone's values. We have also decided that while different genders are allowed to share a room together, 
+they will not be sharing a bed unless they are a couple. 
+
+# Tips
+
+If you bid too high you will pay too much for that room. If you bid too low you will not get the room. 
+The amount that you should bid should determine how much you value the features of that room. For example it may be 
+really important to you that you get a nice bed but for another person it is more important to get a nice view. So you
+would each bid more on different rooms. 
+
+If you want to share a room with a friend then you have to bid more on the room you want to share. Hopefully you will 
+win that auction and both be assigned to that room. 
+
+If you are not there for all the nights you will be disadvantaged because you are paying less by not staying. Because 
+this gives the best rooms to the people who pay the most you will probably not get good rooms. A way to make up for this 
+is to have the total of your bids be more than the price of the house per night. This means that you are willing to 
+pay for more than you are actually staying. Of course you are then subsidizing everyone else's stay, but if you are 
+willing to pay to leave a good room empty, then it is up to you. 
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 
 class Bed:
@@ -27,8 +27,9 @@ class Bed:
 class Room:
     """A room with beds of the same type in it"""
 
-    def __init__(self, beds: List[Bed]):
+    def __init__(self, name: str, beds: List[Bed]):
         """Has beds in it"""
+        self._name = name
         self._beds = beds
 
     def add_bed(self, bed: Bed):
@@ -40,15 +41,19 @@ class Person(object):
     """A person that is going on the trip"""
 
     def __init__(self, name: str, gender: str, days_staying: int):
-        """A person that will be there during the holiday"""
+        """A person that will be there during the holiday
+        :param name: Name of person
+        :param gender: Gender of person ('m' or 'f')
+        :param days_staying: Number of nights the person is staying
+        """
         self._name = name
         self._gender = gender
         self._days_staying = days_staying
-        self._partner = None
+        self.partner = None
         self._bids = {}
 
     def get_name(self):
-        "Name of person"
+        """"Name of person"""
         return self._name
 
     def get_gender(self):
@@ -61,24 +66,33 @@ class Person(object):
 
     def get_partner(self):
         """Partner of person"""
-        return self._partner
+        return self.partner
 
     def add_partner(self, partner):
         """Add a partner to a person"""
-        self._partner = partner
+        self.partner : Person = partner
+        partner.partner = self
 
     def get_bids(self):
         """The bids of this person"""
         return self._bids
 
+    def add_bids(self, bids: Dict[str, float]):
+        """Add bids to a person"""
+        self._bids =
+
 
 class House:
     """House with beds"""
 
-    def __init__(self, price: int):
-        """House that you are staying in. Has beds and a price"""
+    def __init__(self, price: float, nights: int):
+        """House that you are staying in. Has beds and a price
+        :param price: Total price of accomodation
+        :param nights: Number of nights you are staying at accomodation
+        """
         self._price = price
         self.rooms: List[Room] = []
+        self.nights = nights
 
     def get_price(self):
         """ The price per night of the house"""
@@ -91,6 +105,14 @@ class House:
     def add_room(self, room: Room):
         """Add a bed to the house"""
         self.rooms.append(room)
+
+    def get_nights(self):
+        """Number of nights you are staying"""
+        return self.nights
+
+    def price_per_night(self):
+        """Price the group is paying per night"""
+        return self._price / self.nights
 
 
 class Calculator(object):
@@ -110,6 +132,14 @@ class Calculator(object):
         """Returns list of people in the experiment """
         return self._people
 
+    def add_person(self, person: Person):
+        """Add a person to the calculator"""
+        self._people.append(person)
+
+    def add_people(self, people: List[Person]):
+        """Add a list of people to the calculator"""
+        self._people.extend(people)
+
     def get_highest_utility(self):
         """Returns highest utility value found"""
         return self._highest_utility
@@ -123,3 +153,58 @@ if __name__ == '__main__':
     test = Calculator()
 
     # create all the beds
+    bunk_1 = Bed("bunk 1", "single")
+    bunk_2 = Bed("bunk 2", "single")
+    bunk_3 = Bed("bunk 3", "single")
+    bunk_4 = Bed("bunk 4", "single")
+    bunk_5 = Bed("bunk 5", "single")
+    bunk_6 = Bed("bunk 6", "single")
+    single_1 = Bed("single 1", "single")
+    single_2 = Bed("single 2", "single")
+    queen = Bed("Queen", "double")
+    king = Bed("King", "double")
+
+    # Create all the rooms
+    bunk_room = Room("bunk room", [bunk_1, bunk_2, bunk_3, bunk_4, bunk_5, bunk_6])
+    twin_room = Room("twin room", [single_1, single_2])
+    queen_room = Room("queen room", [queen])
+    king_room = Room("king room", [king])
+
+    # Create the house
+    the_house = House(547, 3)
+
+    # Create the people and their bids
+    lewis = Person("lewis", "m", 3)
+    braydon = Person("braydon", "m", 3)
+    tim = Person("tim", "m", 3)
+    jess = Person("jess", "f", 3)
+    daniel = Person("daniel", "m", 3)
+    emma = Person("emma", "f", 3)
+    sam = Person("sam", "m", 3)
+    mel = Person("mel", "f", 3)
+    michael = Person("michael", "m", 2)
+    sarah = Person("sarah", "f", 1)
+
+    # Add rooms to the house
+    the_house.add_room(bunk_room)
+    the_house.add_room(twin_room)
+    the_house.add_room(queen_room)
+    the_house.add_room(king_room)
+
+    # Add house to calculator
+    test.add_house(the_house)
+
+    # Add people to the calculator
+    test.add_people([lewis, braydon, tim, jess, daniel, emma, sam, mel, michael, sarah])
+
+    # Add partner to a person
+    lewis.add_partner(jess)
+
+    # Add bids to a person
+    lewis.add_bids({
+        "bunk room": 100,
+        "twin room": 150,
+        "queen room": 200,
+        "king room": 200
+    })
+
